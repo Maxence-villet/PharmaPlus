e55a474a79d150b4c859d120348731f9486e6687
Adam Ouzgane <adamouzgane@gmail.com>
2025-02-06 18:50:08 +0100

    add Order history files

---
+++ OrderHistory.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderHistory extends OrderService {
+    private List<String> commandes;
+    public OrderHistory() {
+        this.commandes = new ArrayList<>();
+    }
+
+    public void afficherCommandes() {
+        if (commandes.isEmpty()) {
+            System.out.println("Aucune commande dans l'historique.");
+        } else {
+            System.out.println("Historique des commandes :");
+            commandes.forEach(System.out::println);
+        }
+    }
+}
