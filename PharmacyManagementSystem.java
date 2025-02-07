8880f5b48d20b0d688b4ccc46d84c42528d93ab1
Maxence VILLET SCHOUMAKER <137928220+Maxence-villet@users.noreply.github.com>
2025-02-07 09:03:52 +0100

    Add files via upload

---
+++ PharmacyManagementSystem.java
@@ -0,0 +1,24 @@
+package PharmaPlus.Feature_User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PharmacyManagementSystem {
+    public static void main(String[] args) {
+        List<User> userList = new ArrayList<>();
+        
+        User admin = new User("AdminPharma", new AdminRole());
+        admin.performAction();
+        
+        User employee = new User("Employee1", new EmployeeRole());
+        employee.performAction();
+        
+        if (admin.isAdmin()) {
+            AdminRole adminRole = (AdminRole) admin.getRole();
+            adminRole.addUser(employee, userList);
+            adminRole.removeUser(employee, userList);
+        } else {
+            System.out.println("Accès refusé : Seuls les Admin peuvent gérer les utilisateurs.");
+        }
+    }
+}
